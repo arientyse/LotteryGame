626d803be74303edb7cb3c5807e9a7325b1aad53
Ariana Tyson <arianatyson@live.com>
2019-11-19 05:14:47 -0500

    Setting up Class Comments and layout of main method

--- src/edu/gsu/csc1301/lotterygame/LotteryGame.java
+++ src/edu/gsu/csc1301/lotterygame/LotteryGame.java
@@ -1,19 +1,33 @@
-/**
- * 
- */
 package edu.gsu.csc1301.lotterygame;
 
 /**
- * @author Ari
+ * This program will determine if the user is the lottery winner by evaluating if their provided list of numbers
+ * matches the set list of winning numbers
+ * 
+ * @author Ariana Tyson
+ * @version 11/19/2019
  *
  */
-public class LotteryGame {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class LotteryGame 
+{
+
+	static final int NUM_DIGITS = 5;
+	
+	public static void main(String[] args) 
+	{
+		
+		Scanner in = new Scanner(System.in);
+		
+		//Input
+		
+		//Computation
+		
+		//Output
+		
 
 	}
 
